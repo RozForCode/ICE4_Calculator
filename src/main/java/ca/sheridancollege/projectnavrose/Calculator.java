fe81341c88ca0915cdf5582b764a33693405d754
Navrose Singh Johal <johnavro@sheridancollege.ca>
2026-06-01 12:54:58 -0400

    Adding add, substract, multiply, and divide functionality

--- src/main/java/ca/sheridancollege/projectnavrose/Calculator.java
+++ src/main/java/ca/sheridancollege/projectnavrose/Calculator.java
@@ -9,5 +9,22 @@ package ca.sheridancollege.projectnavrose;
  * @author navro
  */
 public class Calculator {
-    
+     public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        }
+        return (double) a / b;
+    }
 }
