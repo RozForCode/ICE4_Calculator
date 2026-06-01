6c53bf9e7e2ebb16a84b0f084168d91d71bdb8d7
Navrose Singh Johal <johnavro@sheridancollege.ca>
2026-06-01 12:52:04 -0400

    Project Initialization

---
+++ src/main/java/ca/sheridancollege/projectnavrose/ProjectNavrose.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.projectnavrose;
+
+/**
+ *
+ * @author navro
+ */
+public class ProjectNavrose {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
